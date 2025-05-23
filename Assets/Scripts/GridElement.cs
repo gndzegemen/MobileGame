431dfe942084b69703b0ac4cff842a94bef978b9
Egemen Gunduz <gndzegemen@gmail.com>
2025-05-23 23:22:27 +0300

    coroutine is added for buttons

--- Assets/Scripts/GridElement.cs
+++ Assets/Scripts/GridElement.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,12 +7,26 @@ public class GridElement : MonoBehaviour
     public string ID;
     public Button button;
     public int waitingTime;
+    public GameObject Front;
+    public GameObject Back;
+    public GameObject Empty;
 
     void Start()
     {
-        ID = Guid.NewGuid().ToString();
+        button?.onClick.AddListener(OnClick);
     }
 
+    void OnClick()
+    {
+        Front.SetActive(false);
+        Back.SetActive(true);
+        StartCoroutine(ReturnImageBack());
+    }
 
-    
+    IEnumerator ReturnImageBack()
+    {
+        yield return new WaitForSeconds(waitingTime);
+        Front.SetActive(true);
+        Back.SetActive(false);
+    }
 }
--- Assets/Scripts/MemoryGame.cs
+++ Assets/Scripts/MemoryGame.cs
@@ -1,24 +1,51 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class MemoryGame : MonoBehaviour
 {
-    public int ElementNumber = 21;
-    public GameObject GridElement;
+    public int ElementNumber = 18;
+    public GameObject GridElementPrefab;
     public Transform Parent;
-    [SerializeField]
-    private List<GameObject> GridElements;
 
+    private List<string> idList = new List<string>();
+    private List<GameObject> gridElements = new List<GameObject>();
 
     private void Start()
     {
+        GenerateIDPairs();
+        Shuffle(idList);
+        SpawnGridElements();
+    }
+
+    void GenerateIDPairs()
+    {
+        int pairCount = ElementNumber / 2;
+
+        for (int i = 0; i < pairCount; i++)
+        {
+            string id = System.Guid.NewGuid().ToString();
+            idList.Add(id);
+            idList.Add(id);
+        }
+    }
+
+    void Shuffle(List<string> list)
+    {
+        for (int i = 0; i < list.Count; i++)
+        {
+            int randIndex = Random.Range(i, list.Count);
+            (list[i], list[randIndex]) = (list[randIndex], list[i]);
+        }
+    }
+
+    void SpawnGridElements()
+    {
         for (int i = 0; i < ElementNumber; i++)
         {
-            var go = Instantiate(GridElement, Parent);
-            
-            GridElements.Add(go);
+            GameObject go = Instantiate(GridElementPrefab, Parent);
+            var element = go.GetComponent<GridElement>();
+            element.ID = idList[i];
+            gridElements.Add(go);
         }
-          
     }
 }
