0ec5aa689425ff4880bdf0e252232ea9db8739b0
Egemen Gunduz <gndzegemen@gmail.com>
2025-05-25 23:06:40 +0300

    progress

--- Assets/Scripts/GridElement.cs
+++ Assets/Scripts/GridElement.cs
@@ -1,32 +1,58 @@
+﻿// GridElement.cs
 using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GridElement : MonoBehaviour
 {
+    public enum State { Back, Front, Empty }
+
     public string ID;
+    public MemoryGame memoryGame;
+
+    [Header("UI Referansları")]
     public Button button;
-    public int waitingTime;
     public GameObject Front;
     public GameObject Back;
     public GameObject Empty;
 
+    public bool isReveal = false;
+    public State CurrentState = State.Back;
+
     void Start()
     {
-        button?.onClick.AddListener(OnClick);
+        button.onClick.AddListener(OnClick);
+        ChangeStatusAsBack();
     }
 
     void OnClick()
     {
-        Front.SetActive(false);
-        Back.SetActive(true);
-        StartCoroutine(ReturnImageBack());
+        memoryGame.RegisterReveal(this);
     }
 
-    IEnumerator ReturnImageBack()
+    public void ChangeStatusAsFront()
     {
-        yield return new WaitForSeconds(waitingTime);
         Front.SetActive(true);
         Back.SetActive(false);
+        Empty.SetActive(false);
+        CurrentState = State.Front;
+        
+    }
+
+
+    public void ChangeStatusAsBack()
+    {
+        Front.SetActive(false);
+        Back.SetActive(true);
+        Empty.SetActive(false);
+        CurrentState = State.Back;
+    }
+
+    public void ChangeStatusAsEmpty()
+    {
+        Front.SetActive(false);
+        Back.SetActive(false);
+        Empty.SetActive(true);
+        CurrentState = State.Empty;
     }
 }
--- Assets/Scripts/MemoryGame.cs
+++ Assets/Scripts/MemoryGame.cs
@@ -1,17 +1,60 @@
-﻿using System.Collections.Generic;
+﻿// MemoryGame.cs
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+using static UnityEngine.GraphicsBuffer;
 
 public class MemoryGame : MonoBehaviour
 {
+    [Header("Grid Ayarları")]
     public int ElementNumber = 18;
     public GameObject GridElementPrefab;
-    public Transform Parent;
+    public RectTransform Parent;
+
+    [Header("Animasyon Ayarları")]
+    [Tooltip("Deste nesnesinin RectTransform'u")]
+    public RectTransform deckTransform;
+    public RectTransform dealCard;
+    [Tooltip("Bir karta gitmesi gereken süre")]
+    public float dealDuration = 0.5f;
+    [Tooltip("Kartlar arası gecikme (s)")]
+    public float dealStagger = 0.1f;
+
+    [Header("Kontrol Ayarları")]
+    [Tooltip("Kartlar eşleşmezse kaç saniye sonra kapanacak?")]
+    public float checkDelay = 1f;
+    public float firstRevealTimeout = 3f;
 
     private List<string> idList = new List<string>();
-    private List<GameObject> gridElements = new List<GameObject>();
+    public List<GridElement> cards = new List<GridElement>();
+    public Stack<GridElement> revealCards = new Stack<GridElement>();
+    public bool canReveal = true;
 
-    private void Start()
+    public GameObject MainPage;
+    public GameObject FinishPage;
+
+    private Coroutine firstRevealTimeoutCoroutine;
+    private Coroutine dealingCoroutine;
+
+
+
+    public void SetupGame()
     {
+
+        idList.Clear();
+        cards.Clear();
+        revealCards.Clear();
+        canReveal = true;
+
+        if (firstRevealTimeoutCoroutine != null)
+        {
+            StopCoroutine(firstRevealTimeoutCoroutine);
+            firstRevealTimeoutCoroutine = null;
+        }
+
+
         GenerateIDPairs();
         Shuffle(idList);
         SpawnGridElements();
@@ -20,7 +63,6 @@ public class MemoryGame : MonoBehaviour
     void GenerateIDPairs()
     {
         int pairCount = ElementNumber / 2;
-
         for (int i = 0; i < pairCount; i++)
         {
             string id = System.Guid.NewGuid().ToString();
@@ -45,7 +87,173 @@ public class MemoryGame : MonoBehaviour
             GameObject go = Instantiate(GridElementPrefab, Parent);
             var element = go.GetComponent<GridElement>();
             element.ID = idList[i];
-            gridElements.Add(go);
+            element.memoryGame = this;
+            element.ChangeStatusAsEmpty(); // Başlangıçta empty (görünmez)
+            element.isReveal = false;
+            element.CurrentState = GridElement.State.Empty;
+            cards.Add(element);
+        }
+
+        // Animasyonu başlat
+        dealingCoroutine = StartCoroutine(DealCardsSequentially());
+    }
+
+    IEnumerator DealCardsSequentially()
+    {
+        // Tüm kartları sırasıyla dağıt
+        for (int i = 0; i < cards.Count; i++)
+        {
+            yield return StartCoroutine(DealSingleCard(cards[i]));
+            yield return new WaitForSeconds(dealStagger);
+        }
+
+        // Tüm kartlar dağıtıldıktan sonra biraz bekle, sonra back'e çevir
+        yield return new WaitForSeconds(1f);
+
+        // Tüm kartları back durumuna çevir
+        foreach (var card in cards)
+        {
+            card.ChangeStatusAsBack();
+            card.isReveal = false;
+            card.CurrentState = GridElement.State.Back;
+        }
+
+        dealingCoroutine = null;
+    }
+
+    IEnumerator DealSingleCard(GridElement targetCard)
+    {
+        // Hedef pozisyonu al
+        var targetTransform = targetCard.GetComponent<RectTransform>();
+        var targetPos = targetTransform.localPosition;
+
+        // DealCard'ı deste pozisyonuna getir ve aktif et
+        dealCard.gameObject.SetActive(true);
+        dealCard.localPosition = deckTransform.localPosition;
+
+        // Animasyon tamamlandığını beklemek için bir flag
+        bool animationComplete = false;
+
+        // Animasyonu başlat
+        dealCard
+            .DOLocalMove(targetPos, dealDuration)
+            .SetEase(Ease.OutBack)
+            .OnComplete(() =>
+            {
+                // Animasyon bittiğinde dealCard'ı gizle
+                dealCard.gameObject.SetActive(false);
+
+                // Hedef kartı front durumuna getir
+                targetCard.ChangeStatusAsFront();
+                targetCard.isReveal = true;
+                targetCard.CurrentState = GridElement.State.Front;
+
+                animationComplete = true;
+            });
+
+        // Animasyonun bitmesini bekle
+        yield return new WaitUntil(() => animationComplete);
+    }
+
+
+    public void RegisterReveal(GridElement card)
+    {
+        if (!canReveal || card.isReveal) return;
+
+        card.ChangeStatusAsFront();
+        card.isReveal = true;
+        revealCards.Push(card);
+
+        if (revealCards.Count == 1)
+        {
+            firstRevealTimeoutCoroutine = StartCoroutine(FirstRevealTimeout());
+        }
+        else if (revealCards.Count == 2)
+        {
+            if (firstRevealTimeoutCoroutine != null)
+            {
+                StopCoroutine(firstRevealTimeoutCoroutine);
+                firstRevealTimeoutCoroutine = null;
+            }
+            StartCoroutine(CheckMatch());
         }
     }
+
+    IEnumerator FirstRevealTimeout()
+    {
+        yield return new WaitForSeconds(firstRevealTimeout);
+
+        // Eğer hâlâ tek kart açıksa, kapat
+        if (revealCards.Count == 1)
+        {
+            var first = revealCards.Pop();
+            first.ChangeStatusAsBack();
+            first.isReveal = false;
+            // Buton etkileşimini geri aç (eğer devre dışı kaldıysa)
+            if (first.button != null)
+                first.button.interactable = true;
+        }
+
+        firstRevealTimeoutCoroutine = null;
+    }
+
+    private IEnumerator CheckMatch()
+    {
+        canReveal = false;
+
+        // Tüm butonları etkileşime kapat
+        foreach (var c in cards)
+            c.button.interactable = false;
+
+        // Bekle (animasyon için zaman)
+        yield return new WaitForSeconds(checkDelay);
+
+        // İki kartı al
+        var first = revealCards.Pop();
+        var second = revealCards.Pop();
+
+        if (first.ID == second.ID)
+        {
+            // Eşleşme: boş slot yap
+            first.ChangeStatusAsEmpty();
+            second.ChangeStatusAsEmpty();
+
+            cards.Remove(first);
+            cards.Remove(second);
+
+            if(cards.Count == 0)
+            {
+                MainPage.gameObject.SetActive(false);
+                FinishPage.gameObject.SetActive(true);
+                ResetGame();
+            }
+        }
+        else
+        {
+            // Eşleşmez: geri kapat
+            first.ChangeStatusAsBack();
+            second.ChangeStatusAsBack();
+            first.isReveal = false;
+            second.isReveal = false;
+        }
+
+        // Sadece hâlâ kapalı (empty olmayan) kartlara izin ver
+        foreach (var c in cards)
+            if (!c.isReveal && c.CurrentState != GridElement.State.Empty)
+                c.button.interactable = true;
+
+        canReveal = true;
+    }
+
+    public void ResetGame()
+    {
+        // Sahnedeki eski kartları temizle
+        foreach (Transform child in Parent)
+            Destroy(child.gameObject);
+
+        // Yeniden kurulum
+        SetupGame();
+    }
+
+
 }
